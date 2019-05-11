701a306f741fbfec9c9853be6f30ca10bb351745
Raju Ahmed Shetu <shetu2153@gmail.com>
2019-05-11 10:58:51 +0600

    another route added

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello_world():
     return jsonify(dict(message='Hello World'))
 
+@app.route('/home')
+def hello_home():
+    return jsonify(dict(message='Hello Home'))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
