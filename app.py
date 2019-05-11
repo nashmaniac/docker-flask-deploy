223c5b68231fd1831dcc3a131ff29edb5ae2c616
Raju Ahmed Shetu <shetu2153@gmail.com>
2019-05-11 10:25:37 +0600

    initialized

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import jsonify
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return jsonify(dict(message='Hello World'))
+
+
+if __name__ == '__main__':
+    app.run()
